b622ba872df738070e82b3b718465d8da4f8324f
Gregory <gregorycedro@gmail.com>
2025-01-22 10:08:45 -0300

    876. Middle of the Linked List

---
+++ Linked List/876_middle_of_the_linked_list.js
@@ -0,0 +1,10 @@
+var middleNode = function(head) {
+    let ahead = head;
+
+    while(ahead && ahead.next){
+        ahead = ahead.next.next;
+        head = head.next;
+    }
+
+    return head;
+};
